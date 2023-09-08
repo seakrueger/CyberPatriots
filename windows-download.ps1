ed1e2b9b64f116b91c3a29b09ee533a04a65bf03
Sean Krueger <skrueger2270@gmail.com>
2023-09-08 16:45:52 -0500

    Add install scripts

---
+++ windows-download.ps1
@@ -0,0 +1,3 @@
+﻿Invoke-WebRequest -Uri https://github.com/skrueger-ftc/CyberPatriots/archive/refs/heads/main.zip -OutFile cyberpatriot-scripts.zip
+Expand-Archive -Path cyberpatriot-scripts.zip -DestinationPath scripts
+Set-Location -Path scripts\CyberPatriots-main\windows
