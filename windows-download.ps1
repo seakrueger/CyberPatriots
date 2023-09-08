0b199e6357f07411ebbb122c47be77c10bdcab61
Sean Krueger <skrueger2270@gmail.com>
2023-09-08 17:03:16 -0500

    Update download script

--- windows-download.ps1
+++ windows-download.ps1
@@ -1,5 +1,6 @@
 ﻿Invoke-WebRequest -Uri https://github.com/skrueger-ftc/CyberPatriots/archive/refs/heads/main.zip -OutFile cyberpatriot-scripts.zip
 Expand-Archive -Path cyberpatriot-scripts.zip -DestinationPath scripts
-Remove-Item -Path cyberpatriot-scripts.zip
 Move-Item -Path scripts\CyberPatriots-main\windows -Destination windows-script
+Remove-Item -Path cyberpatriot-scripts.zip
+Remove-Item -Path scripts -Recurse
 Set-Location -Path windows-script
